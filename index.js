695172f3d5e41fa13f591943420039b1df7ab04e
Hrishi <rishiyadav2108@gmail.com>
2024-10-07 16:22:30 +0530

    updated condition for odd

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let a = [20,45,88,55,77,55,42,,65,5,2,3,2,66,6652,663,4]
+let a = [20,45,88,55,77,55,42,,65,5,2,55,555,5455,84551,55445,3,2,66,6652,663,4]
 for(i=0;i<a.length;i++)
 {
     if(i % 2 ==0)
     {
         console.log(i , "Its an Even Number")
     }
-    else if(i % 2 == 1)
+    else if(i % 2 !== 0)
     {
         console.log(i , "Its an odd number")
     }
