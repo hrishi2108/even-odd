13b6e5aa80d2456443af74abdcbc24c3cc8daa28
Hrishi <rishiyadav2108@gmail.com>
2024-10-07 16:17:19 +0530

    It find even and odd

---
+++ index.js
@@ -0,0 +1,12 @@
+let a = [20,45,88,55,77,55,42,,65,5,2,3,2,66,6652,663,4]
+for(i=0;i<a.length;i++)
+{
+    if(i % 2 ==0)
+    {
+        console.log(i , "Its an Even Number")
+    }
+    else if(i % 2 == 1)
+    {
+        console.log(i , "Its an odd number")
+    }
+}
